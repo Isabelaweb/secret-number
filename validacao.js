04a5f8ed269f2fc972a5f27a5c562bdd9f1ce285
Isabela Oliveira <isabelaoliveira@Isabela.local>
2023-11-10 15:11:36 +0000

    validacao de numeros

--- validacao.js
+++ validacao.js
@@ -1,4 +1,26 @@
 function verificaSeValorValido(chute){
+
+    const numeros = [
+        {name: 'zero zero', value: 0,},
+        {name: 'zero', value: 0},
+        {name:'00', value: 0,},
+        {name:'um', value: 1,},
+        {name:'dois', value: 2,},
+        {name:'três', value: 3,},
+        {name:'quatro', value: 4,},
+        {name:'cinco', value: 5,},
+        {name:'seis', value: 6,},
+        {name:'sete', value: 7,},
+        {name:'oito', value: 8,},
+        {name:'nove', value: 9,},
+        {name:'dez', value: 10}
+    ]
+   const porExtenso = numeros.find((numero) => numero.name === chute)
+    
+    if (porExtenso){
+        chute = porExtenso.value 
+    }
+
     const numero = +chute
     console.log("chute", numero)
     
